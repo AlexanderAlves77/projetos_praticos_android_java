512e2228b7045d41cabc470edfe75faf6ce1d7c7
Alexander Alves <vendas77.rj@gmail.com>
2024-10-03 08:17:51 -0300

    Metodo salvar AppListaCurso

--- AppListaCurso/app/src/main/java/devandroid/aeca/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/aeca/applistacurso/controller/PessoaController.java
@@ -1,20 +1,24 @@
 package devandroid.aeca.applistacurso.controller;
 
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import devandroid.aeca.applistacurso.database.ListaVipDB;
 import devandroid.aeca.applistacurso.model.Pessoa;
 import devandroid.aeca.applistacurso.view.MainActivity;
 
-public class PessoaController {
+public class PessoaController extends ListaVipDB {
 
     SharedPreferences preferences;
     SharedPreferences.Editor listaVip;
     public static final String NOME_PREFERENCES = "pref_listavip";
 
     public PessoaController(MainActivity mainActivity) {
+        super(mainActivity);
+
         preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
         listaVip = preferences.edit();
     }
@@ -26,6 +30,8 @@ public class PessoaController {
     }
 
     public void salvar(Pessoa pessoa) {
+        ContentValues dados = new ContentValues();
+
         Log.i("MVC Controller", "Salvo: " + pessoa.toString());
 
         listaVip.putString("primeiroNome:", pessoa.getPrimeiroNome());
@@ -33,6 +39,13 @@ public class PessoaController {
         listaVip.putString("cursoDesejado:", pessoa.getCursoDesejado());
         listaVip.putString("telefoneContato:", pessoa.getTelefoneContato());
         listaVip.apply();
+
+        dados.put("primeiroNome", pessoa.getPrimeiroNome());
+        dados.put("sobreNome", pessoa.getSobreNome());
+        dados.put("cursoDesejado", pessoa.getCursoDesejado());
+        dados.put("telefoneContato", pessoa.getTelefoneContato());
+
+        salvarObjeto("Pessoa", dados);
     }
 
     public Pessoa buscar(Pessoa pessoa) {
