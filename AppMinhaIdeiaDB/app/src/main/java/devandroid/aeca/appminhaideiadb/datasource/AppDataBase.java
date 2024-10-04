b9a73cb675b15ee4547b97cdb9ffe93488ff2e6a
Alexander Alves <vendas77.rj@gmail.com>
2024-10-04 17:43:04 -0300

    Criando tabela clientes

--- AppMinhaIdeiaDB/app/src/main/java/devandroid/aeca/appminhaideiadb/datasource/AppDataBase.java
+++ AppMinhaIdeiaDB/app/src/main/java/devandroid/aeca/appminhaideiadb/datasource/AppDataBase.java
@@ -6,21 +6,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import devandroid.aeca.appminhaideiadb.core.AppUtil;
+import devandroid.aeca.appminhaideiadb.datamodel.ClienteDataModel;
 
 public class AppDataBase extends SQLiteOpenHelper {
 
     public static final String  DB_NAME =  "AppMinhaIdeia.sqlite";
     public static final int DB_VERSION =  1;
 
+    SQLiteDatabase db;
+
     public AppDataBase(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
-
         Log.d(AppUtil.TAG, "AppDataBase: Criando Banco de Dados");
+
+        db = getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(ClienteDataModel.criarTabela());
+        Log.d(AppUtil.TAG, "onCreate: Tabela Cliente " + ClienteDataModel.criarTabela());
     }
 
     @Override
