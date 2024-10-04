1020feb6623e5b503072f1ab970fe853741c04c4
Alexander Alves <vendas77.rj@gmail.com>
2024-10-04 08:52:05 -0300

    Estrutura App Minha Ideia

---
+++ AppMinhaIdeiaDB/app/src/main/java/devandroid/aeca/appminhaideiadb/SplashActivity.java
@@ -0,0 +1,47 @@
+package devandroid.aeca.appminhaideiadb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    String TAG = "APP_MINHA_IDEIA";
+    int waitingTime = 1000 * 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Log.d(TAG, "onCreate: Tela Splash Carregada...");
+
+        changeScreen();
+
+    }
+
+    private void changeScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent changeOfScreen = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(changeOfScreen);
+                finish();
+            }
+        }, waitingTime);
+    }
+
+}
