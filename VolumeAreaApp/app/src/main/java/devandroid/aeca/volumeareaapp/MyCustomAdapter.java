d44218a8047bbdd80845d3a540214b061ff39f20
Alexander Alves <vendas77.rj@gmail.com>
2024-10-05 11:40:21 -0300

    Create MyCustomAdapter

--- VolumeAreaApp/app/src/main/java/devandroid/aeca/volumeareaapp/MyCustomAdapter.java
+++ VolumeAreaApp/app/src/main/java/devandroid/aeca/volumeareaapp/MyCustomAdapter.java
@@ -1,11 +1,15 @@
 package devandroid.aeca.volumeareaapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
@@ -23,5 +27,36 @@ public class MyCustomAdapter extends ArrayAdapter<Shape> {
     public static class MyViewHolder {
         TextView shapeName;
         ImageView shapeImg;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        Shape shapes = getItem(position);
+        MyViewHolder myViewHolder;
+
+        if (convertView == null) {
+            myViewHolder = new MyViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(
+                    R.layout.grid_item_layout,
+                    parent,
+                    false
+            );
+
+            myViewHolder.shapeName = (TextView) convertView.findViewById(R.id.textView);
+            myViewHolder.shapeImg = (ImageView) convertView.findViewById(R.id.imageView);
+
+            convertView.setTag(myViewHolder);
+        }
+        else {
+            myViewHolder = (MyViewHolder) convertView.getTag();
+        }
+
+        myViewHolder.shapeName.setText(shapes.getShapeName());
+        myViewHolder.shapeImg.setImageResource(shapes.getShapeImg());
+
+        return convertView;
     }
 }
