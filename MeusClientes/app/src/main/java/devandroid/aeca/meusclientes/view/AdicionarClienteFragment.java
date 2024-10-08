558fefe84af297d93c6b59bbd4e8364d693a6edb
Alexander Alves <vendas77.rj@gmail.com>
2024-10-08 12:03:05 -0300

    Tela Adicionar Clientes

--- MeusClientes/app/src/main/java/devandroid/aeca/meusclientes/view/AdicionarClienteFragment.java
+++ MeusClientes/app/src/main/java/devandroid/aeca/meusclientes/view/AdicionarClienteFragment.java
@@ -3,6 +3,8 @@ package devandroid.aeca.meusclientes.view;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +12,12 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import devandroid.aeca.meusclientes.R;
+import devandroid.aeca.meusclientes.api.AppUtil;
 import devandroid.aeca.meusclientes.controller.ClienteController;
 import devandroid.aeca.meusclientes.model.Cliente;
 
@@ -89,7 +93,79 @@ public class AdicionarClienteFragment extends Fragment {
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                clienteController.incluir(novoCliente);
+                boolean isDataOk = true;
+
+                if (TextUtils.isEmpty(editNome.getText())) {
+                    isDataOk = false;
+                    editNome.setError("Digite o nome completo...");
+                    editNome.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editTelefone.getText())) {
+                    isDataOk = false;
+                    editTelefone.setError("Digite o telefone...");
+                    editTelefone.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editEmail.getText())) {
+                    isDataOk = false;
+                    editEmail.setError("Digite o email...");
+                    editEmail.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editCep.getText())) {
+                    isDataOk = false;
+                    editCep.setError("Digite o cep...");
+                    editCep.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editLogradouro.getText())) {
+                    isDataOk = false;
+                    editLogradouro.setError("Digite o logradouro (avenida, estrada, rua)...");
+                    editLogradouro.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editNumero.getText())) {
+                    isDataOk = false;
+                    editNumero.setError("Digite o número...");
+                    editNumero.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editBairro.getText())) {
+                    isDataOk = false;
+                    editBairro.setError("Digite o bairro...");
+                    editBairro.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editCidade.getText())) {
+                    isDataOk = false;
+                    editCidade.setError("Digite a cidade...");
+                    editCidade.requestFocus();
+                }
+
+                if (TextUtils.isEmpty(editEstado.getText())) {
+                    isDataOk = false;
+                    editEstado.setError("Digite o estado (UF)...");
+                    editEstado.requestFocus();
+                }
+
+                if (isDataOk) {
+                    novoCliente.setNome(editNome.getText().toString());
+                    novoCliente.setTelefone(editTelefone.getText().toString());
+                    novoCliente.setEmail(editEmail.getText().toString());
+                    novoCliente.setCep(Integer.parseInt(editCep.getText().toString()));
+                    novoCliente.setLogradouro(editLogradouro.getText().toString());
+                    novoCliente.setNumero(editNumero.getText().toString());
+                    novoCliente.setBairro(editBairro.getText().toString());
+                    novoCliente.setCidade(editCidade.getText().toString());
+                    novoCliente.setEstado(editEstado.getText().toString());
+                    novoCliente.setTermosDeUso(chkTermos.isChecked());
+
+                    clienteController.incluir(novoCliente);
+                }
+                else {
+                    Log.i("log_add_cliente", "onClick: Dados incorreto...");
+                }
             }
         });
     }
