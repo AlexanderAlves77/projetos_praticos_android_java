e9bc0ba8cb1f5a55b858893541da539444f97b62
Alexander Alves <vendas77.rj@gmail.com>
2026-03-03 20:14:13 -0300

    Payment, one-to-one association

--- webservices/src/main/java/com/fulldevstacks/webservices/config/TestConfig.java
+++ webservices/src/main/java/com/fulldevstacks/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fulldevstacks.webservices.entities.Category;
 import com.fulldevstacks.webservices.entities.Order;
 import com.fulldevstacks.webservices.entities.OrderItem;
+import com.fulldevstacks.webservices.entities.Payment;
 import com.fulldevstacks.webservices.entities.Product;
 import com.fulldevstacks.webservices.entities.User;
 import com.fulldevstacks.webservices.entities.enums.OrderStatus;
@@ -78,5 +79,9 @@ public class TestConfig implements CommandLineRunner
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
