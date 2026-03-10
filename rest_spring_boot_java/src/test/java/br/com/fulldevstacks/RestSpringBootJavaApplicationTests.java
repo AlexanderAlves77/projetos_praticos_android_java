be678d4ccd31b3e5ae6f4999ddd28a6b838ac503
Alexander Alves <vendas77.rj@gmail.com>
2026-03-10 10:36:05 -0300

    Initialize project rest spring

---
+++ rest_spring_boot_java/src/test/java/br/com/fulldevstacks/RestSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fulldevstacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
