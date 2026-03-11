5a462ea03d151b0ede11b36a100b8556863fd54c
Alexander Alves <vendas77.rj@gmail.com>
2026-03-11 13:00:10 -0300

    Modify BookController

--- book-service/src/main/java/br/com/fulldevstacks/controller/BookController.java
+++ book-service/src/main/java/br/com/fulldevstacks/controller/BookController.java
@@ -1,14 +1,11 @@
 package br.com.fulldevstacks.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.fulldevstacks.dto.Exchange;
 import br.com.fulldevstacks.environment.InstanceInformationService;
